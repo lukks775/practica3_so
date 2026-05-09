551e63a3f4f4fb3cb0cfb3e0a25b3121a3dc99b6
Lucia Castellanos Paz <lukkskass775@gmail.com>
2026-05-09 17:45:22 +0200

    arreglo .cpp

--- src/cliente.cpp
+++ src/cliente.cpp
@@ -22,44 +22,43 @@ return sockfd;
 void configurarServidor(sockaddr_in &servaddr){ //&para modificar la variable original
 servaddr.sin_family=AF_INET;
 servaddr.sin_port=htons(12345);//puerto del servidor
-if(inet_pton(AF_INET,"192.168.0.148",&servaddr.sin_addr)<=0){
+if(inet_pton(AF_INET,"127.0.0.1",&servaddr.sin_addr)<=0){
 cerr<<"direccion invalida o no soportada"<<endl;
 }
 }
 
 //para conectar el cliente con el servidor
 int conectarServidor(int sockfd,sockaddr_in &servaddr){
-if(connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr))==-1){//si no se conecta da error
+if(connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr))==-1){//si no conecta
 cerr<<"Error al conectar"<<endl;
 return -1;
 }//si se conecta bien
 return 0;
 }
 
-
-//esta funcion recive  los datos enviados por el servidor
-void recibirDatos(int sockfd,vector<float> &floats){
-float buffer; //variable temporal para recibir cada float
+//esta funcion recive los datos enviados por el servidor
+void recibirDatos(int sockfd,vector<unsigned char> &datos){
+unsigned char buffer; //variable temporal para recibir cada dato
 while(recv(sockfd,&buffer,sizeof(buffer),0)>0){
-floats.push_back(buffer);
+datos.push_back(buffer);
 }
 }
 
-//esta fuvncion guarda  los datos recibidos en un archivo .txt
-void guardarDatos(vector<float> &floats){
-ofstream f("temperaturas.txt"); //crea el archivo temperatuas.txt
-for(float x:floats){//recorre los datos del vector
-f<<x<<endl;
+//esta funcion guarda los datos recibidos en un archivo .txt
+void guardarDatos(vector<unsigned char> &datos){
+ofstream f("temperaturas.txt"); //crea el archivo temperaturas.txt
+for(unsigned char x:datos){//recorre los datos del vector
+f<<(int)x<<endl;
 }
 f.close();
 }
 
 
-//muestra r por la pantalla los datos recibidos
-void imprimirDatos(vector<float> &floats){
-cout<<"Numeros  recibidos:"<<endl;
-for(float f:floats){
-cout<<f<<endl;
+//muestra por la pantalla los datos recibidos
+void imprimirDatos(vector<unsigned char> &datos){
+cout<<"Datos recibidos:"<<endl;
+for(unsigned char f:datos){
+cout<<(int)f<<endl;
 }
 }
 
@@ -69,15 +68,23 @@ int sockfd=crearSocket();
 if(sockfd==-1){
 return 1;
 }
+
 sockaddr_in servaddr;
+
 configurarServidor(servaddr);
+
 if(conectarServidor(sockfd,servaddr)==-1){
 return 1;
 }
-vector<float> floats;
-recibirDatos(sockfd,floats);
-guardarDatos(floats);
-imprimirDatos(floats);
+
+vector<unsigned char> datos;
+
+recibirDatos(sockfd,datos);
+
+guardarDatos(datos);
+
+imprimirDatos(datos);
+
 close(sockfd);
 
 return 0;
